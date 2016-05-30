6d459056611d2a6611d85e5f12f909de87bffde6
Travis Briggs <audiodude@gmail.com>
2016-05-30 11:27:27 -0700

    Move to more reliable async font loading.

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 var SQUARE_SIZE = 300;
 
-$(function() {
+function adjust_text() {
   var div = $('#text');
 
   div.html(div.text().replace(' ', '&nbsp;'));
 
-	var size = 30;
+  var size = 30;
   while(div[0].scrollHeight <= SQUARE_SIZE) {
-		div.css('font-size', size + 'px');
-		size += 0.1;
+    div.css('font-size', size + 'px');
+    size += 0.1;
   }
   div.css('font-size', (size - 0.2) + 'px');
 
   var lh = 65;
   while(div[0].scrollHeight <= SQUARE_SIZE) {
-		div.css('line-height', lh + 'px');
-		lh += 0.1;
+    div.css('line-height', lh + 'px');
+    lh += 0.1;
   }
   div.css('line-height', (lh - 0.1) + 'px');
-});
+}
+
+WebFontConfig.fontactive = adjust_text;
